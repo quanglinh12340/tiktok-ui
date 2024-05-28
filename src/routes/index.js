109f59beed320e608a5359e3f9d4991f110baec7
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-05-28 16:25:17 +0700

    build Default Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ import Home from '@/pages/Home'
 import Following from "@/pages/Following";
 import Profile from '@/pages/Profile';
 import Upload from "@/pages/Upload";
-import Search from "@/pages/Search";
+import Search from "@/pages/Search"
 
 //Public routes
 const publicRoutes = [
