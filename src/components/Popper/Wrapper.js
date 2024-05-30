141bf03a289725469bf94c83f8890d6185125593
quanglinh12340 <149453502+quanglinh12340@users.noreply.github.com>
2024-05-30 14:02:31 +0700

    build UI Header- Menu and Menu Items

--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -4,9 +4,9 @@ import style from './Popper.module.scss'
 
 const cx = classNames.bind(style)
 
-function Wrapper({ children }) {
+function Wrapper({ children, className }) {
     return (
-        <div className={cx('wrapper')}>
+        <div className={cx('wrapper', className)}>
             {children}
         </div>
     )
